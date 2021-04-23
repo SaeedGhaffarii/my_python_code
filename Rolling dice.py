79e1eacaf444d5551d1b7be77000b7a2fe942871
SaeedGhaffarii <83059573+SaeedGhaffarii@users.noreply.github.com>
2021-04-23 11:47:14 +0430

    Add files via upload

---
+++ Rolling dice.py
@@ -0,0 +1,26 @@
+# region Rolling dice 6000 times without using list
+# frequency1 = frequency2 = frequency3 = frequency4 = frequency5 = frequency6 = 0
+#
+# for _ in range(6000):
+#     face = randrange(1, 7)
+#
+#     if face == 1:
+#         frequency1 += 1
+#     elif face == 2:
+#         frequency2 += 1
+#     elif face == 3:
+#         frequency3 += 1
+#     elif face == 4:
+#         frequency4 += 1
+#     elif face == 5:
+#         frequency5 += 1
+#     else:
+#         frequency6 += 1
+#
+# print("Face    Frequency")
+# print(f"{1:4}{frequency1:13}")
+# print(f"{2:4}{frequency2:13}")
+# print(f"{3:4}{frequency3:13}")
+# print(f"{4:4}{frequency4:13}")
+# print(f"{5:4}{frequency5:13}")
+# print(f"{6:4}{frequency6:13}")
